9f705c7a6b944d97ca901f3dd02afa94bf9aa40a
Mariangel <mariangel.yajure@gmail.com>
2022-03-17 20:30:09 -0430

    obteniendo entrada del usuario. En python 2 raw_input y en python 3 input

---
+++ leccion01/entrada_usuario.py
@@ -0,0 +1,7 @@
+#Funcion input para procesar la entrada del usuario
+print('Como se llama?')
+
+#En python 2 se utiliza raw_input, en python 3 input()
+nombre = raw_input()
+print('Me alegro de conocerte ' + nombre)
+print("Fin del programa")

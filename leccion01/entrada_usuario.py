81ac1b5bd5153ad4bb933ea54b39294809eb69a2
Mariangel <mariangel.yajure@gmail.com>
2022-03-18 12:12:53 -0430

    Conversión en la entrada de datos

--- leccion01/entrada_usuario.py
+++ leccion01/entrada_usuario.py
@@ -4,4 +4,13 @@ print('Como se llama?')
 #En python 2 se utiliza raw_input, en python 3 input()
 nombre = raw_input()
 print('Me alegro de conocerte ' + nombre)
+print('Ahora escribe el primer numero para sumar')
+
+numero1 = int(raw_input())
+numero2 = int(raw_input("Escribe el segundo numero: "))
+
+resultado = numero1 + numero2
+
+print("El resultado es: " + str(resultado))
+
 print("Fin del programa")

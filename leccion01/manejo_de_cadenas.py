4548faa247b82701024ba900a16d5aa9b0f2fadf
Mariangel <mariangel.yajure@gmail.com>
2022-03-17 19:40:30 -0430

    Corrigiendo

--- leccion01/manejo_de_cadenas.py
+++ leccion01/manejo_de_cadenas.py
@@ -5,10 +5,15 @@ comentario = "The Best Rock Band"
 
 #print ("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)
 
-print ("Mi grupo favorito es:", miGrupoFavorito, comentario) #Otra manera de concatenar
+print("Mi grupo favorito es:", miGrupoFavorito, comentario)
+
+#Otra manera de concatenar
 
 numero1 = "1"
 numero2 = "2"
 
-print (numero1 + numero2) #Asi no hace la suma sino que coloca la literal
-print (int(numero1)+int(numero2)) #Asi si realiza la suma
+print(numero1 + numero2)
+#Asi no hace la suma sino que coloca la literal
+
+print(int(numero1)+int(numero2))
+#Asi si realiza la suma
